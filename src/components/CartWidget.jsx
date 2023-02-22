80876459fb7a709165168d4c073349a21b20d874
Guillermo Mayoral <espart-187@outlook.com>
2023-02-21 21:34:41 -0600

    correccion class

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,7 @@ const CartWidget = () => {
     return (
         <div>
             <h4>Carrito de compras</h4>
-            <span class="material-symbols-outlined">
+            <span className="material-symbols-outlined">
                 shopping_cart
             </span>
             <span>3</span>
