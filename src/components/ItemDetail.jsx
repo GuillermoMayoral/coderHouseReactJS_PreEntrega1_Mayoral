3e29580bc49921a5fc3ef214a7e04a556c853b96
Guillermo Mayoral <espart-187@outlook.com>
2023-03-31 19:06:09 -0600

    finalizacion de firestore

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -14,47 +14,75 @@ import {
 } from "@chakra-ui/react";
 import ItemCount from './ItemCount'
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import Loading from './Loading';
 
 
 
 
-const ItemDetail = ({ Data }) => {
+const ItemDetail = () => {
+
+    const [producto, setProducto] = useState([]);
+    const [load, setLoad] = useState(true);
+
+
+
+    useEffect(() => {
+        const db = getFirestore();
+
+        const itemId = doc(db, "calzado", `${id}`)
+        getDoc(itemId).then((snapshot) => {
+            if (snapshot.exists()) {
+                const docs = snapshot.data();
+                console.log(docs);
+                setProducto(docs);
+                setLoad(false);
+            }
+        })
+
+
+    }, [])
+
+    console.log(producto);
+
+
     const { id } = useParams();
-    const filter = Data.filter((datos) => datos.id == id);
 
+    if (load) {
+        return <Loading />
+    }
 
     return (
         <>
-            {filter.map((datos) => (
-                <div key={datos.id}>
-                    <Center>
-                        <Card maxW="sm">
-                            <CardBody>
-                                <Image src={datos.img} />
-                                <Stack mt="6" spacing="3">
-                                    <Heading size="md">{datos.name}</Heading>
-                                    <Text>{datos.description}</Text>
-                                    <Text color="blue.600" fontSize="2xl">
-                                        {"$" + datos.price + ".00"}
-                                    </Text>
-                                </Stack>
-                            </CardBody>
-                            <Divider />
-                            <CardFooter>
-                                <Center className="card-footer">
-                                    <ItemCount
-                                        stock={datos.stock}
-                                        id={datos.id}
-                                        price={datos.price}
-                                        name={datos.name}
-                                        img={datos.img}
-                                    />
-                                </Center>
-                            </CardFooter>
-                        </Card>
-                    </Center>
-                </div>
-            ))}
+            <div key={producto.id}>
+                <Center>
+                    <Card maxW="sm">
+                        <CardBody>
+                            <Image src={producto.img} />
+                            <Stack mt="6" spacing="3">
+                                <Heading size="md">{producto.name}</Heading>
+                                <Text>{producto.description}</Text>
+                                <Text color="blue.600" fontSize="2xl">
+                                    {"$" + producto.price + ".00"}
+                                </Text>
+                            </Stack>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter>
+                            <Center className="card-footer">
+                                <ItemCount
+                                    stock={producto.stock}
+                                    id={producto.id}
+                                    price={producto.price}
+                                    name={producto.name}
+                                    img={producto.img}
+                                />
+                            </Center>
+                        </CardFooter>
+                    </Card>
+                </Center>
+            </div>
         </>
     )
 }
